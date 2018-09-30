fa8831db06b59b69cb4944169005cb2c9cf40514
Nikita Valkevich <n.valkevich@icloud.com>
2018-09-30 23:46:49 +0300

    first com

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var arr = [];
+  var open = [];
+  var close = [];
+
+  function openBracket (char){
+  	return open.indexOf(char) >= 0;
+  }
+
+  function closeBracket (char){
+  	return close.indexOf(char) >= 0;
+  }
+  
+  function peek(){
+  	return arr[arr.length - 1];
+  }
+
+  for (var i = 0; i < bracketsConfig.length; i++ ){
+  	open.push(bracketsConfig[i][0]);
+  	close.push(bracketsConfig[i][1]);
+  }
+
+  for (var i = 0; i < str.length; i++){
+  	var char = str[i];
+
+  	if(openBracket(char) && closeBracket(char)){
+  		if (peek() === char){
+  			arr.pop();
+  		}
+  		else{
+  			arr.push(char);
+  		}
+
+  	}
+  	else if (openBracket(char)){
+  		arr.push(char);
+  	}
+  	else if (closeBracket(char)){
+  		if (peek() === open[close.indexOf(char)]) {
+                arr.pop();
+            } else {
+                return false;
+            }
+        } else{
+        	return false;
+        }
+  }
+  	return arr.length === 0;
+
 }
